dc3d72f2dac5f9cab9979265d68e901781debb85
Andrew <slonryba@gmail.com>
2020-06-28 19:24:44 +0300

    calc is works! his not beauty, but works!

--- handlers.js
+++ handlers.js
@@ -12,14 +12,34 @@ const back = () => {
     return document.form.display.value = str;
 };
 
-const eval = (str) => {
+const eval = (str) => {    
+    const signs = ['+', '-', '*', '/'];
+
+    let arg1, arg2, oper;
+
     for (const every of str) {
-        if ((typeof(+every) !== 'number') && (every !== '.')) {
-            
-        }
-    }
-}
+      if (signs.includes(every)) {
+        oper = every;
+      }
+    };
+    
+    arg1 = +str.slice(0, str.indexOf(oper));
+    arg2 = +str.slice(str.indexOf(oper) + 1);
+    
+    switch(oper) {
+      case '+': return arg1 + arg2;
+      break;
+      case '-': return arg1 - arg2;
+      break;
+      case '*': return arg1 * arg2;
+      break;
+      case '/': return arg1 / arg2;
+      default : return 'how you do it?';
+    };
+  };
+
 
 const equal = () => {
     let exp = document.form.display.value;
-}
+    return document.form.display.value = eval(exp);
+};
